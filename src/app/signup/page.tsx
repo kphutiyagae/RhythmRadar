492bf8783c969813204d874904ecff10ac1524b5
kphutiyagae <kopanophutiyagae@gmail.com>
2024-03-26 14:07:23 +0200

    feat(none): :lipstick: Added hero image to signup.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import React from 'react';
+import Image from 'next/image';
+import SignupHeroImage from '/public/images/signup-hero.jpg'
 
 export default function Signup(){
   return (
     <div>
-      <p>Music Page</p>
+      <Image src={SignupHeroImage} objectFit={'contain'} height={window.innerHeight} className='object-center object-cover flex justify-center items-center' alt='Hero Image for signup page'/>
     </div>
   );
 };
