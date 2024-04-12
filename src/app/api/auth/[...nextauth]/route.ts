64c4e5eda28f9729b051e3164ac4dcc0a62b1d74
kphutiyagae <kopanophutiyagae@gmail.com>
2024-04-12 11:26:37 +0200

    feat(none): :sparkles: Auth works! session data pulling through.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,16 @@
-// import NextAuth from 'next-auth';
-// import Spotify from 'next-auth/providers/spotify';
-// import { environments } from '@/app/environments/dev.environment';
-// import Google from 'next-auth/providers/google';
-// export const authOptions = {
-//   providers: [
-//     // Spotify({
-//     //   clientId: environments.SPOTIFY_PUBLIC_API_KEY,
-//     //   clientSecret: environments.SPOTIFY_PRIVATE_API_KEY
-//     // })
-//     Google({
-//       clientId: process.env.
-//     })
-//   ]
-// }
-//
-// export default NextAuth(authOptions)
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import Github from "next-auth/providers/github";
 import Spotify from 'next-auth/providers/spotify';
 
+console.log({
+  scopes: process.env.AUTH_SPOTIFY_API_SCOPES,
+  scopesReplaced: process.env.AUTH_SPOTIFY_API_SCOPES.toString().replaceAll(',','+')
+})
 export const authOptions = {
   providers: [
     Spotify({
       clientId: process.env.AUTH_SPOTIFY_PUBLIC_API_KEY,
       clientSecret: process.env.AUTH_SPOTIFY_PRIVATE_API_KEY,
-
+      authorization: `https://accounts.spotify.com/authorize?scope=${process.env.AUTH_SPOTIFY_API_SCOPES.toString().replaceAll(',','+')}`
     })
   ]
 }
@@ -33,5 +18,3 @@ export const authOptions = {
 const handler = NextAuth(authOptions);
 
 export {handler as GET, handler as POST};
-
-// export default NextAuth(authOptions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { getServerSession } from 'next-auth';
+
+export default async function Home() {
+	// const {data: session, status} = useSession();
+
+	const session = await getServerSession()
+
+	console.log({session: session});
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<div>Landing Page</div>
+			<p>Status: {session?.user?.name ?? 'Not logged in'}</p>
 		</main>
 	);
 }
