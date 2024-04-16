2adace4123807997e9d3387ad92d234eb0ecdeda
kphutiyagae <kopanophutiyagae@gmail.com>
2024-04-16 12:55:04 +0200

    feat(none): :sparkles: Added more methods to spotify service.

--- src/app/services/spotify/spotify.service.ts
+++ src/app/services/spotify/spotify.service.ts
@@ -14,9 +14,7 @@ export async function getUserTopTracks() {
     }).then( response => response.json())
   }
 }
-
 export async function getUserTopArtists() {
-  // console.log({code: accessToken})
   const session = await getSession();
   if(session?.accessToken === undefined) return;
 
@@ -26,11 +24,80 @@ export async function getUserTopArtists() {
     }
   }).then( response => response.json())
 }
+
+export async function getUserPlaylists(userId: string) {
+  const session = await getSession();
+  if(session?.accessToken === undefined || session?.user?.name === undefined) return;
+
+  return fetch(`${environments.SPOTIFY_API_BASE_URL}/users/${userId}/playlists`, {
+    headers: {
+      Authorization: `Authorization: Bearer ${session.accessToken}`
+    }
+  }).then( response => response.json())
+}
+
+export async function getRecommendedPlaylists() {
+  const session = await getSession();
+  if (session?.accessToken === undefined) return;
+
+  return fetch(`${environments.SPOTIFY_API_BASE_URL}/browse/featured-playlists`, {
+    headers: {
+      Authorization: `Authorization: Bearer ${session.accessToken}`
+    }
+  }).then(response => response.json())
+}
+
 export async function getGenresList(accessToken: string) {
   console.log({code: accessToken})
   return fetch(`${environments.SPOTIFY_API_BASE_URL}/me/top/artists`, {
     headers: {
       Authorization: `Authorization: Bearer ${accessToken}`
+    }
+  }).then( response => response.json())
+}
+
+export async function getCurrentUserProfile() {
+  // console.log({code: accessToken})
+  const session = await getSession();
+  if(session?.accessToken === undefined) return;
+
+  return fetch(`${environments.SPOTIFY_API_BASE_URL}/me`, {
+    headers: {
+      Authorization: `Authorization: Bearer ${session.accessToken}`
+    }
+  }).then( response => response.json())
+}
+
+export async function getCategoryPlaylists(categoryID: string) {
+  const session = await getSession();
+  if(session?.accessToken === undefined) return;
+
+  return fetch(`${environments.SPOTIFY_API_BASE_URL}/browse/categories/${categoryID}/playlists`, {
+    headers: {
+      Authorization: `Authorization: Bearer ${session.accessToken}`
+    }
+  }).then( response => response.json())
+}
+
+
+export async function search(query: string) {
+  const session = await getSession();
+  if(session?.accessToken === undefined || query.length === 0) return;
+
+  return fetch(`${environments.SPOTIFY_API_BASE_URL}/search?${query}`, {
+    headers: {
+      Authorization: `Authorization: Bearer ${session.accessToken}`
+    }
+  }).then( response => response.json())
+}
+
+export async function getRecommendations() {
+  const session = await getSession();
+  if(session?.accessToken === undefined || query.length === 0) return;
+
+  return fetch(`${environments.SPOTIFY_API_BASE_URL}/recommendations`, {
+    headers: {
+      Authorization: `Authorization: Bearer ${session.accessToken}`
     }
   }).then( response => response.json())
 }
