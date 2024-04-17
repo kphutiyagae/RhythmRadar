e607e91ef113bed702891836403d559004514604
kphutiyagae <kopanophutiyagae@gmail.com>
2024-04-17 12:24:45 +0200

    feat(none): :lipstick: Added links to nav.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,14 +10,22 @@ function Header() {
   return (
     <div className='py-4 px-12 text-white w-full h-full flex flex-row justify-between items-baseline'>
       <ul className='w-3/4 h-full flex flex-row justify-between mr-2 font-black'>
-        <li className='nav-item'>
-          <div>
-            <p>Rhythm <span className='-ml-1 text-primary'>Radar</span></p>
-          </div>
-        </li>
-        <li className='nav-item'>Music</li>
-        <li className='nav-item'>Podcast</li>
-        <li className='nav-item'>Radio</li>
+        <a href='/'>
+          <li>
+            <div>
+              <p>Rhythm <span className='-ml-1 text-primary'>Radar</span></p>
+            </div>
+          </li>
+        </a>
+        <a href='/music'>
+          <li className='nav-item'>Music</li>
+        </a>
+        <a href='/podcast'>
+          <li className='nav-item'>Podcast</li>
+        </a>
+        <a href='/radio'>
+          <li className='nav-item'>Radio</li>
+        </a>
       </ul>
       <>
         {session?.user !== undefined ? <div className='logged-in-button'> <p className='text-lg font-black'>{session?.user?.name ?? 'User'}</p> </div> : <a href='/api/auth/signin'><button className='nav-button'>Sign In</button></a>}
