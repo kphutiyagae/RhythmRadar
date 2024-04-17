e96f73be4f5a4d832588187df17c979cbe9c4643
kphutiyagae <kopanophutiyagae@gmail.com>
2024-04-17 15:27:58 +0200

    feat(none): :construction: Added Globe to Radio page

--- src/app/radio/page.tsx
+++ src/app/radio/page.tsx
@@ -1,9 +1,33 @@
+'use client'
 import React from 'react';
+import Globe from 'react-globe.gl';
+import globeTexture from '../../../public/earth-dark.jpg';
 
 export default function Radio(){
+  const myData = [
+    {
+      lat: 29.953204744601763,
+      lng: -90.08925929478903,
+      altitude: 0.4,
+      color: '#00ff33',
+    },
+    {
+      lat: 28.621322361013092,
+      lng: 77.20347613099612,
+      altitude: 0.4,
+      color: '#ff0000',
+    },
+    {
+      lat: -43.1571459086602,
+      lng: 172.72338919659848,
+      altitude: 0.4,
+      color: '#ffff00',
+    },
+  ];
+
   return (
-    <div>
-      <p>Radio Page</p>
+    <div className='cursor-move'>
+      <Globe globeImageUrl={globeTexture.src} pointsData={myData} pointAltitude='altitude' pointColor='color'  />
     </div>
   );
 };
