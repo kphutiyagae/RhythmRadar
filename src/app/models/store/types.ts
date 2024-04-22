8e1c2f350d2d68e5f7949181c764d84046fe6064
kphutiyagae <kopanophutiyagae@gmail.com>
2024-04-22 15:04:20 +0200

    feat(models) : :sparkles: Added more to RadioStoreType.

--- src/app/models/store/types.ts
+++ src/app/models/store/types.ts
@@ -2,7 +2,7 @@ import { Station } from 'radio-browser-api';
 
 export interface RadioSearchParams {
   filterBy: {[key: string]: string}[]
-  sortBy: 'asc' | 'desc' | 'alphabetical' | 'alphabetical-rev' | 'listens';
+  sortBy: 'asc' | 'desc' | 'alphabetical' | 'alphabetical-rev' | 'listens' | '';
 }
 export interface RadioStoreType {
   currentCoordinates: [number, number];
@@ -10,4 +10,13 @@ export interface RadioStoreType {
   stations: Station[];
   searchTerm: string;
   searchParams: RadioSearchParams;
+  updateRadioStations: () => void;
+  clearRadioStations: () => void;
+  updateCurrentCoordinates: (coordinates: [number, number]) => void;
+  clearCurrentCoordinates: () => void;
+  updateCurrentRadioStation: (station: Station) => void;
+  updateSearchTerm: (searchTerm: string) => void;
+  clearSearchTerm: () => void;
+  updateSearchParams: (params: RadioSearchParams) => void;
+  clearSearchParams: () => void;
 }
